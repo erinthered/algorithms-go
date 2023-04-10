9d8b31c24dfd044b78c4ebec64f10666e75bc12b
Erin Williams <erinbesswilliams@gmail.com>
2023-04-10 11:56:42 -0400

    Add recursive bubblesort and update test

--- sort/bubble/bubble.go
+++ sort/bubble/bubble.go
@@ -2,7 +2,9 @@ package bubble
 
 import "fmt"
 
-func bubbleSort(arr []int) {
+type bubbleSort func([]int) []int
+
+func bubbleSortIterative(arr []int) []int {
 	for i := 0; i < len(arr); i++ {
 		for j := 0; j < len(arr)-1; j++ {
 			if arr[j+1] < arr[j] {
@@ -10,6 +12,24 @@ func bubbleSort(arr []int) {
 			}
 		}
 	}
+	return arr
+}
+
+func bubbleSortRecursive(arr []int) []int {
+	// Cover trivial and base cases: length of array is 0 or 1
+	if len(arr) <= 1 {
+		return arr
+	}
+	// If next element is smaller than current element, swap them
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i+1] < arr[i] {
+			arr[i], arr[i+1] = arr[i+1], arr[i]
+		}
+	}
+	// Recursive call to bubblesort
+	bubbleSortRecursive(arr[:len(arr)-1])
+
+	return arr
 }
 
 // RunBubbleSort checks each element of an array with the next element and swaps them if next is smaller
@@ -29,17 +49,21 @@ func RunBubbleSort() {
 		{1, 2, 3, 4, 5},
 		{1, 2, 18, 23, 112, 300, 501},
 	}
-	var sorted bool
-	for index, input := range inputs {
-		sorted = true
-		fmt.Printf("Input: %v\t::", input)
-		bubbleSort(input)
-		for i, v := range input {
-			if v != expected[index][i] {
-				sorted = false
-				break
+	testBubbleSort := func(bs bubbleSort) {
+		for index, input := range inputs {
+			fmt.Printf("Testing input: %v\n", input)
+			output := bs(input)
+			sorted := true
+			for i, v := range output {
+				if v != expected[index][i] {
+					sorted = false
+					break
+				}
 			}
+			fmt.Printf("Sorted correctly: %t\t:: Output: %v\n\n", sorted, input)
 		}
-		fmt.Printf("Sorted correctly: %t\t:: Output: %v\n", sorted, input)
 	}
+
+	testBubbleSort(bubbleSortIterative)
+	testBubbleSort(bubbleSortRecursive)
 }
