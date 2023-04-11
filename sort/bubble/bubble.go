056d091b0f4656b548e3b262fd58866236b6e98b
Erin Williams <erinbesswilliams@gmail.com>
2023-04-11 15:52:22 -0400

    Bubble Sort

--- sort/bubble/bubble.go
+++ sort/bubble/bubble.go
@@ -1,10 +1,6 @@
 package bubble
 
-import "fmt"
-
-type bubbleSort func([]int) []int
-
-func bubbleSortIterative(arr []int) []int {
+func SortIterative(arr []int) []int {
 	for i := 0; i < len(arr); i++ {
 		for j := 0; j < len(arr)-1; j++ {
 			if arr[j+1] < arr[j] {
@@ -15,7 +11,7 @@ func bubbleSortIterative(arr []int) []int {
 	return arr
 }
 
-func bubbleSortRecursive(arr []int) []int {
+func SortRecursive(arr []int) []int {
 	// Cover trivial and base cases: length of array is 0 or 1
 	if len(arr) <= 1 {
 		return arr
@@ -27,43 +23,7 @@ func bubbleSortRecursive(arr []int) []int {
 		}
 	}
 	// Recursive call to bubblesort
-	bubbleSortRecursive(arr[:len(arr)-1])
+	SortRecursive(arr[:len(arr)-1])
 
 	return arr
-}
-
-// RunBubbleSort checks each element of an array with the next element and swaps them if next is smaller
-// Runs in quadratic time (O(n^2))
-func RunBubbleSort() {
-	inputs := [][]int{
-		{},
-		{1},
-		{1, 2, 3, 4, 5},
-		{5, 4, 3, 2, 1},
-		{23, 18, 501, 112, 2, 1, 300},
-	}
-	expected := [][]int{
-		{},
-		{1},
-		{1, 2, 3, 4, 5},
-		{1, 2, 3, 4, 5},
-		{1, 2, 18, 23, 112, 300, 501},
-	}
-	testBubbleSort := func(bs bubbleSort) {
-		for index, input := range inputs {
-			fmt.Printf("Testing input: %v\n", input)
-			output := bs(input)
-			sorted := true
-			for i, v := range output {
-				if v != expected[index][i] {
-					sorted = false
-					break
-				}
-			}
-			fmt.Printf("Sorted correctly: %t\t:: Output: %v\n\n", sorted, input)
-		}
-	}
-
-	testBubbleSort(bubbleSortIterative)
-	testBubbleSort(bubbleSortRecursive)
 }
--- sort/main.go
+++ sort/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "algorithms-go/sort/bubble"
+import (
+	"algorithms-go/sort/bubble"
+	"algorithms-go/sort/selection"
+	"fmt"
+)
+
+type sortFunction func([]int) []int
 
 func main() {
-	bubble.RunBubbleSort()
+	fmt.Println("Testing bubble sort")
+	testSort(bubble.SortIterative)
+	testSort(bubble.SortRecursive)
+
+	fmt.Println("Testing selection sort")
+	testSort(selection.Sort)
+}
+
+func testSort(fun sortFunction) {
+	inputs := [][]int{
+		{},
+		{1},
+		{1, 2, 3, 4, 5},
+		{5, 4, 3, 2, 1},
+		{23, 18, 501, 112, 2, 1, 300},
+	}
+	expected := [][]int{
+		{},
+		{1},
+		{1, 2, 3, 4, 5},
+		{1, 2, 3, 4, 5},
+		{1, 2, 18, 23, 112, 300, 501},
+	}
+	for index, input := range inputs {
+		fmt.Printf("Testing input %d: %v\n", index, input)
+		output := fun(input)
+		sorted := true
+		for i, v := range output {
+			if v != expected[index][i] {
+				sorted = false
+				break
+			}
+		}
+		fmt.Printf("Sorted correctly: %t\t:: Output: %v\n\n", sorted, input)
+	}
 }
